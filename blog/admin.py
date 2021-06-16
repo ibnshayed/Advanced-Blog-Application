2ee42efc130fef110e01765b840f761cb2ce4a14
ibnshayed <emranibnshayed@gmail.com>
2021-06-16 16:05:01 +0600

    summernote added

---
+++ blog/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+
+from .models import (Author, Blog, Catagory, Comment, Contact, EmailSignUp,
+                     Reply, Tag)
+
+
+class CatAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Catagory,CatAdmin)
+
+# @admin.register(Blog)
+class BlogAdmin(SummernoteModelAdmin):
+	summernote_fields = ('detail',)
+
+admin.site.register(Blog, BlogAdmin)
+
+admin.site.register(Comment)
+admin.site.register(Reply)
+admin.site.register(Tag)
+admin.site.register(EmailSignUp)
+admin.site.register(Contact)
+
+
+# made by Nazrul Islam Yeasin 
+# Facebook : facebook.com/yeariha.farsin
+# Github : github.com/yeazin
+# website : yeazin.github.io
---
+++ dashboard/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Author
+
+admin.site.register(Author)
+
+# made by Nazrul Islam Yeasin 
+# Facebook : facebook.com/yeariha.farsin
+# Github : github.com/yeazin
+# website : yeazin.github.io
